2874f06acbd6d362ad1a8519ce4d752f8c9c6637
CarolineMVB <92635955+CarolineMVB@users.noreply.github.com>
2022-02-09 16:06:14 +0100

    fix section-0 inclusion

--- locallib.php
+++ locallib.php
@@ -29,11 +29,12 @@ use pdf;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once("$CFG->dirroot/config.php");
-require_once("$CFG->dirroot/course/lib.php");
-require_once("$CFG->libdir/filelib.php");
+require_once "$CFG->dirroot/config.php";
+require_once "$CFG->dirroot/course/lib.php";
+require_once "$CFG->libdir/filelib.php";
 
-class custom_course_progress_lib {
+class custom_course_progress_lib
+{
 
     private $progresscourses;
     private $idlecourses;
@@ -41,12 +42,13 @@ class custom_course_progress_lib {
     /**
      * Constructor.
      */
-    public function __construct($context) {
+    public function __construct($context)
+    {
         $this->context = $context;
     }
 
-
-    public function prepare_content($userid) {
+    public function prepare_content($userid)
+    {
         $courses = enrol_get_all_users_courses($userid, true);
         $progresscourses = array();
         $idlecourses = array();
@@ -117,6 +119,7 @@ class custom_course_progress_lib {
                             }
                         }
                     }
+
                     if ($section->section > 0) {
                         if ($hasprogress) {
                             $trackedsection->progress = round($trackedsection->modcompleted / $trackedsection->modcount * 100, 1);
@@ -153,15 +156,18 @@ class custom_course_progress_lib {
      *
      * @return \block_custom_course_progress\output\main_content custom_course_progress main_content renderer
      */
-    public function generate_content() {
+    public function generate_content()
+    {
         return new \block_custom_course_progress\output\main_content($this->progresscourses, $this->idlecourses, $this->context);
     }
 
-    public static function cmp($a, $b) {
+    public static function cmp($a, $b)
+    {
         return strcmp($a->fullname, $b->fullname);
     }
 
-    public static function get_course_image($course) {
+    public static function get_course_image($course)
+    {
         global $CFG;
 
         $url = "";
@@ -169,8 +175,8 @@ class custom_course_progress_lib {
         foreach ($courseinlist->get_course_overviewfiles() as $file) {
             $isimage = $file->is_valid_image();
             $url = file_encode_url("$CFG->wwwroot/pluginfile.php",
-                    '/'. $file->get_contextid(). '/'. $file->get_component(). '/'.
-                    $file->get_filearea(). $file->get_filepath(). $file->get_filename(), !$isimage);
+                '/' . $file->get_contextid() . '/' . $file->get_component() . '/' .
+                $file->get_filearea() . $file->get_filepath() . $file->get_filename(), !$isimage);
         }
 
         return $url;
@@ -182,7 +188,8 @@ class custom_course_progress_lib {
      * @param int $userid
      * @param int $attemptnumber (-1 means latest attempt)
      */
-    private static function hash($contextid, $userid) {
+    private static function hash($contextid, $userid)
+    {
         return sha1($contextid . '_' . $userid);
     }
 
@@ -190,17 +197,19 @@ class custom_course_progress_lib {
      * Save the completed PDF to the given file
      * @param string $filename the filename for the PDF (including the full path)
      */
-    private static function save_pdf($pdf, $filename) {
+    private static function save_pdf($pdf, $filename)
+    {
         $olddebug = error_reporting(0);
         $pdf->Output($filename, 'F');
         error_reporting($olddebug);
     }
 
-    public function make_export($userid, $filename) {
+    public function make_export($userid, $filename)
+    {
         global $CFG, $DB, $OUTPUT;
 
-        require_once($CFG->libdir . '/pdflib.php');
-        
+        require_once $CFG->libdir . '/pdflib.php';
+
         $tmpdir = \make_temp_directory('block_custom_course_progress/export/' . self::hash($this->context->id, $userid));
         $combined = $tmpdir . '/' . $filename;
         $user = $DB->get_record('user', array('id' => $userid), '*', MUST_EXIST);
@@ -220,7 +229,7 @@ class custom_course_progress_lib {
             $pdf->setHeaderMargin(10);
             $pdf->setHeaderFont(array('helvetica', '', 11));
             $pdf->setHeaderData('blocks/custom_course_progress/pix/logo-upr.jpg', 40, get_string('export_title', 'block_custom_course_progress'), $username);
-            
+
             $pdf->SetTitle(get_string('export_title', 'block_custom_course_progress'));
             $pdf->SetSubject(get_string('export_title', 'block_custom_course_progress'));
             $pdf->SetKeywords('');
@@ -236,7 +245,7 @@ class custom_course_progress_lib {
             $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
             // Get the CSS.
             $styles = '<style>' . file_get_contents($CFG->dirroot . '/blocks/custom_course_progress/styles.css') . '</style>';
-            
+
             $pdf->AddPage();
             $pdf->setJPEGQuality(100);
 
@@ -252,35 +261,35 @@ class custom_course_progress_lib {
             $y = 20;
             $w = 80;
             $h = 40;
-            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255,255,255));
+            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255, 255, 255));
             $imgdata = file_get_contents($logos[0]);
-            $pdf->Image('@'.$imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
+            $pdf->Image('@' . $imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
 
             $w = 60;
             $x = 135;
-            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255,255,255));
+            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255, 255, 255));
             $imgdata = file_get_contents($logos[1]);
-            $pdf->Image('@'.$imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
+            $pdf->Image('@' . $imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
 
             $pdf->SetXY(15, 145);
-            $pdf->writeHTMLCell(0,0, 15, 100, '<h1>Rapport</h1><h1>'.$username.'</h1>', 0, 0, false, true, 'C', true);
+            $pdf->writeHTMLCell(0, 0, 15, 100, '<h1>Rapport</h1><h1>' . $username . '</h1>', 0, 0, false, true, 'C', true);
             $x = 75;
             $y = 130;
             $imgdata = file_get_contents($logos[2]);
-            $pdf->Image('@'.$imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'C', false, false);
+            $pdf->Image('@' . $imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'C', false, false);
 
             $x = 15;
             $y = 235;
             $w = 45;
-            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255,255,255));
+            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255, 255, 255));
             $imgdata = file_get_contents($logos[3]);
-            $pdf->Image('@'.$imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
+            $pdf->Image('@' . $imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
 
             $x = 125;
             $w = 70;
-            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255,255,255));
+            $pdf->Rect($x, $y, $w, $h, 'F', array(), array(255, 255, 255));
             $imgdata = file_get_contents($logos[4]);
-            $pdf->Image('@'.$imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
+            $pdf->Image('@' . $imgdata, $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, 'L', false, false);
 
             $pdf->setPrintHeader(true);
             $pdf->AddPage();
@@ -291,9 +300,9 @@ class custom_course_progress_lib {
             if (!$firstusedate->timecreated) {
                 $firstusedate = $user->firstaccess;
             }
-            
+
             $datenow = new \DateTime('now', new \DateTimeZone(\core_date::normalise_timezone($CFG->timezone)));
-            $html  = "<h3>Synthèse SONATE de $username</h3>";
+            $html = "<h3>Synthèse SONATE de $username</h3>";
             $html .= "<p><br><br></p>";
             $html .= "<em><h4>$user->city</h4></em>";
             $html .= "<p><br><br><br></p>";
@@ -306,19 +315,19 @@ class custom_course_progress_lib {
             foreach ($this->progresscourses as $course) {
                 if (isset($course) && $course->courseprogress > 0) {
                     $html .= "<h4>$course->fullname</h4>";
-                    $html .= "<p>A réalisé ".$course->courseprogress."%</p>";
+                    $html .= "<p>A réalisé " . $course->courseprogress . "%</p>";
                     $validated = 0;
                     $inprogress = 0;
                     foreach ($course->sections as $section) {
                         if ($section->progress > 99) {
                             $validated++;
-                        } else if ($section->progress > 0) {
+                        } else if ($section->progress >= 0) {
                             $inprogress++;
                         }
                     }
                     $noprogress = count($course->noprogresssections);
                     $total = count($course->sections) + $noprogress;
-                    $html .= "<p>Total de jalons : ".$total."</p>";
+                    $html .= "<p>Total de jalons : " . $total . "</p>";
                     $html .= "<p>Jalons complétés : $validated</p>";
                     $html .= "<p>Jalons commencés (mais encore incomplets) : $inprogress</p>";
                     $html .= "<p>Jalons non travaillés : $noprogress</p>";
@@ -342,16 +351,16 @@ class custom_course_progress_lib {
             $fs = get_file_storage();
 
             $fileinfo = array(
-                'contextid' => $this->context->id,  // ID of context
-                'component' => 'block_custom_course_progress',     // usually = table name
-                'filearea' => 'content',    // usually = table name
-                'itemid' => 0,              // usually = ID of row in table
-                'filepath' => '/',          // any path beginning and ending in /
-                'filename' => $filename);   // any filename
+                'contextid' => $this->context->id, // ID of context
+                'component' => 'block_custom_course_progress', // usually = table name
+                'filearea' => 'content', // usually = table name
+                'itemid' => 0, // usually = ID of row in table
+                'filepath' => '/', // any path beginning and ending in /
+                'filename' => $filename); // any filename
 
             $file = $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'],
-                    $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename']);
-                
+                $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename']);
+
             if ($file) {
                 // Delete the old file first
                 $file->delete();
@@ -359,45 +368,47 @@ class custom_course_progress_lib {
 
             $file = $fs->create_file_from_pathname($fileinfo, $tmpdir . '/' . $filename);
 
-            $path = '/'.$this->context->id.'/block_custom_course_progress/content/'.$file->get_itemid().$file->get_filepath().$filename;
+            $path = '/' . $this->context->id . '/block_custom_course_progress/content/' . $file->get_itemid() . $file->get_filepath() . $filename;
             return moodle_url::make_file_url('/pluginfile.php', $path);
         }
     }
 
-    private function get_gradeitems($userid, $courseid, $moduleinstance, $modname) {
+    private function get_gradeitems($userid, $courseid, $moduleinstance, $modname)
+    {
         global $DB;
 
-        $sql = "SELECT {grade_items}.id AS id, {course}.id as course_id, 
-                {grade_items}.itemname, {grade_items}.itemtype, {grade_items}.itemmodule, 
-                ROUND({grade_grades}.finalgrade, 2) AS finalgrade, 
+        $sql = "SELECT {grade_items}.id AS id, {course}.id as course_id,
+                {grade_items}.itemname, {grade_items}.itemtype, {grade_items}.itemmodule,
+                ROUND({grade_grades}.finalgrade, 2) AS finalgrade,
                 ROUND({grade_grades}.rawgrademax, 0) AS grademax,
-                FROM_UNIXTIME({grade_grades}.timecreated) AS date_created, 
-                FROM_UNIXTIME({grade_grades}.timemodified) AS date_modified, 
-                SUBSTRING({grade_grades}.feedback, 1, 15) 
-                FROM {grade_grades} 
-                JOIN {user} ON {grade_grades}.userid = {user}.id 
-                JOIN {grade_items} ON {grade_grades}.itemid = {grade_items}.id 
-                JOIN {course} ON {grade_items}.courseid = {course}.id 
+                FROM_UNIXTIME({grade_grades}.timecreated) AS date_created,
+                FROM_UNIXTIME({grade_grades}.timemodified) AS date_modified,
+                SUBSTRING({grade_grades}.feedback, 1, 15)
+                FROM {grade_grades}
+                JOIN {user} ON {grade_grades}.userid = {user}.id
+                JOIN {grade_items} ON {grade_grades}.itemid = {grade_items}.id
+                JOIN {course} ON {grade_items}.courseid = {course}.id
                 WHERE {user}.id = ?
-                AND {course}.id = ? 
-                AND {grade_items}.iteminstance = ? 
-                AND {grade_items}.itemmodule = ? 
-                AND {grade_items}.itemtype = 'mod' 
-                AND {grade_grades}.finalgrade > 0 
+                AND {course}.id = ?
+                AND {grade_items}.iteminstance = ?
+                AND {grade_items}.itemmodule = ?
+                AND {grade_items}.itemtype = 'mod'
+                AND {grade_grades}.finalgrade > 0
                 ORDER BY {course}.id, {grade_items}.id, {grade_grades}.timemodified LIMIT 1;";
-            
+
         return $DB->get_record_sql($sql, array($userid, $courseid, $moduleinstance, $modname));
     }
 
-    private function get_first_use_date($userid, $courses) {
+    private function get_first_use_date($userid, $courses)
+    {
         global $DB;
 
         $sql = "SELECT {logstore_standard_log}.timecreated
-                FROM {logstore_standard_log} 
+                FROM {logstore_standard_log}
                 WHERE {logstore_standard_log}.userid = " . $userid . "
-                AND {logstore_standard_log}.courseid IN (" . implode(',',  array_column($courses, 'id')) . ")
+                AND {logstore_standard_log}.courseid IN (" . implode(',', array_column($courses, 'id')) . ")
                 ORDER BY {logstore_standard_log}.timecreated ASC LIMIT 1;";
-            
+
         return $DB->get_record_sql($sql);
     }
 }
